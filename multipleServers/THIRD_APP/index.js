d161e82a125e9deedb7d73819d26399bc94d219d
AishJoshi-2002 <aishjoshi2002@gmail.com>
2024-08-03 19:21:16 +0530

    Multiple Servers

---
+++ multipleServers/THIRD_APP/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');  // Import the cors middleware
+const app = express();
+const socket = require('socket.io');
+
+// Use the cors middleware
+app.use(cors());
+
+const server = app.listen(8005, () => {
+    console.log('Server is running on port 8005');
+});
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'));
+
+let io = socket(server, {
+    cors: {
+        origin: "http://localhost:3000",  // Allow requests from this origin
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
+
+io.on('connection', function(socket) {
+    console.log('User connected: ' + socket.id);
+    socket.on('eventCreated', function(data) {
+        console.log('response for Server2: ' + data.message);
+        io.emit('responseFromServer2', data);
+    });
+});
